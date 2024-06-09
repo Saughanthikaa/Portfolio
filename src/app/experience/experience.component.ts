dc2e969d3fd9cf5dfea459ba64a7a85475155b90
Saughanthikaa <Saughanthikaa.ke@gmail.com>
2024-06-09 19:45:22 +0530

    changed content in experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -12,6 +12,6 @@ export class ExperienceComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  timeLine = [{ year: 'Smartail Pvt Ltd', time:'Jun 2023 - Nov 2023' ,loc:'Chennai', detail: 'Developed a status tracker website to monitor the lifecycle of question papers, including receipt, correction, and dispatch to schools.Implemented features to assign deadlines and designate personnel for correcting and reviewing answer papers.Generated comprehensive reports on a daily and monthly basis to provide insights into workflow and performance metrics.',tools:'Tools Used : Angular , Java(Rest APIs) , MongoDB' },
+  timeLine = [{ year: 'Smartail Pvt Ltd', time:'Jun 2023 - Nov 2023' ,loc:'Chennai', detail: 'Developed a status tracker website to monitor the lifecycle of question papers, including receipt, correction, and dispatch to schools. Implemented features to assign deadlines and designate personnel for correcting and reviewing answer papers.Generated comprehensive reports on a daily and monthly basis to provide insights into workflow and performance metrics.',tools:'Tools Used : Angular , Java(Rest APIs) , MongoDB' },
     ]
 }
